67a00f56f8968c5c16f628812fb10b55a139085c
Florian Lauch <florian.lauch@epitech.eu>
2023-04-05 11:53:40 +0200

    feat(packets): command structure

--- include/packets.h
+++ include/packets.h
@@ -14,9 +14,32 @@
 typedef char packet_header_t;
 
 typedef enum packet_type_e {
+    PACKET_COMMAND,
     PACKET_MESSAGE,
     PACKET_USER,
 } packet_type_t;
+
+typedef enum packet_command_e {
+    COMMAND_LOGIN,
+    COMMAND_LOGOUT,
+    COMMAND_USERS,
+    COMMAND_USER,
+    COMMAND_SEND,
+    COMMAND_MESSAGES,
+    COMMAND_SUBSCRIBE,
+    COMMAND_SUBSCRIBED,
+    COMMAND_UNSUBSCRIBE,
+    COMMAND_USE,
+    COMMAND_CREATE,
+    COMMAND_LIST,
+    COMMAND_INFO,
+} packet_command_t;
+
+typedef struct command_packet_s {
+    packet_command_t command;
+    size_t data_size;
+    char data[0];
+} command_packet_t;
 
 typedef struct message_packet_s {
     int code;
