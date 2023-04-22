cf51d9a93b7fa677995e638f68ed4422a2a2256c
Florian Lauch <33784129+EdenComp@users.noreply.github.com>
2023-04-22 12:05:52 +0200

    fix: null check

--- server/src/util/checks.c
+++ server/src/util/checks.c
@@ -27,6 +27,9 @@ bool is_user_subscribed(user_t *user, team_t *team)
 {
     uuid_t *uuid = NULL;
 
+    if (user == NULL) {
+      return false;
+    }
     SLIST_FOREACH(uuid, team->users, next) {
         if (strcmp(uuid->uuid, user->uuid) == 0)
             return true;
